2973b04cb68625a164c34a54e55129884cdad8ea
Andres Ramirez <gokutanve@gmail.com>
2022-11-27 20:41:13 -0400

    change some visual,add erarer mode, rainbow mode and select size function fix

--- javascript.js
+++ javascript.js
@@ -1,8 +1,35 @@
-const container = document.getElementById("grid-container");
+const container = document.getElementById('grid-container');
 const btnRestart = document.getElementById('restart');
+const userSize = document.getElementById('size');
+const rainbowMode = document.getElementById('color');
+const eraserMode = document.getElementById('eraser');
+let isHover = false;
+let isRainbow = false;
+let eraser = false;
+const size = userSize.addEventListener('keydown',function(e){
+    if(e.keyCode === 13){
+        const size = e.target.value * 1;
+        if (size <= 100){
+        restartGrid();
+        makeGrid(size,size);
+        } else {
+        alert('error! insert a number lower than 100!');
+        }
+    } else {
+        return;
+    }
+});
 
+function rainbow(){
+        var num = Math.round(0xffffff * Math.random());
+        var r = num >> 16;
+        var g = num >> 8 & 255;
+        var b = num & 255;
+        return 'rgb(' + r + ', ' + g + ', ' + b + ')';
+} 
 function restartGrid(){
     const cell = document.querySelectorAll('.grid-cell');
+    makeGrid(16,16);
     cell.forEach(item => {
         item.style.backgroundColor = 'white';
     })
@@ -10,15 +37,36 @@ function restartGrid(){
 function makeGrid(rows, cols) {
     for(let i = 0; i < (rows * cols) ; i++){
         const cell = document.createElement('div');
+        container.style.gridTemplateColumns = `repeat(${rows}, 1fr)`;
+        container.style.gridTemplateRows = `repeat(${cols}, 1fr)`;
         cell.className = 'grid-cell';
         cell.setAttribute('id','grid-cell' + i);
         
         container.appendChild(cell);
     }
 }
-makeGrid(16, 16);
-container.addEventListener('mouseover',function(e){
-    const newCell = e.target;
-    newCell.style.backgroundColor = 'black';
+container.addEventListener('mousedown',function(e){
+    isHover = true;
 });
-btnRestart.addEventListener('click',restartGrid)
+
+container.addEventListener('mousemove',function(e){
+    const cell = e.target;
+    if (isHover && isRainbow){
+        cell.style.backgroundColor = rainbow();
+    } else if (eraser && isHover){
+        cell.style.backgroundColor = 'white';
+    } else if (isHover){
+        cell.style.backgroundColor = 'black';
+    }
+});
+container.addEventListener('mouseup',function(e){
+    isHover = false;
+});
+btnRestart.addEventListener('click',restartGrid);
+rainbowMode.addEventListener('click',function(){
+    isRainbow = !isRainbow;
+});
+eraserMode.addEventListener('click',function(){
+    eraser =!eraser;
+});
+makeGrid(16,16);
